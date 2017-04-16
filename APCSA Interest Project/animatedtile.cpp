8db09c6902be2d4c18ac7d25d95ac14f5e9a79ed
ChristopherB <officialchrisb@icloud.com>
2017-04-15 23:36:50 -0500

    Updated layout

--- APCSA Interest Project/animatedtile.cpp
+++ APCSA Interest Project/animatedtile.cpp
@@ -2,17 +2,17 @@
 #include "graphics.h"
 
 AnimatedTile::AnimatedTile(std::vector<Vector2> tilesetPositions, int duration,
-                           SDL_Texture* tileset, Vector2 size, Vector2 position) :
-Tile(tileset, size, tilesetPositions.at(0), position),
-_tilesetPositions(tilesetPositions),
-_duration(duration),
-_tileToDraw(0)
+                            SDL_Texture* tileset, Vector2 size, Vector2 position) :
+                            Tile(tileset, size, tilesetPositions.at(0), position),
+                            _tilesetPositions(tilesetPositions),
+                            _duration(duration),
+                            _tileToDraw(0)
 {}
 
 void AnimatedTile::update(int elapsedTime) {
     //Timer code
     if (this->_amountOfTime <= 0) {
-        if (this->_tileToDraw == this->_tilesetPositions.size() -1) {
+        if (this->_tileToDraw == this->_tilesetPositions.size() - 1) {
             this->_tileToDraw = 0;
         }
         else {
